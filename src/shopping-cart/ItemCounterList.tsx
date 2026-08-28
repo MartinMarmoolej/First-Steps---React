c41f2efee43d138c678f04b0d2d903063578d023
Martin Marmolejo <martinalejandro.marmolejo@gmail.com>
2026-08-28 16:50:01 -0700

    Se trabajo con archivos CSS

--- src/shopping-cart/ItemCounterList.tsx
+++ src/shopping-cart/ItemCounterList.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+
+import './ItemCounter.css'
+import styles from './ItemCounter.module.css'
+
 interface Props {
     name: string;
     quantity?: number;
@@ -24,16 +28,19 @@ export const ItemCounter = ({ name, quantity = 1 }: Props) => {
 
 
     return (
-        <section style={{
-            display: "flex",
-            alignItems: "center",
-            gap: 10,
-            marginTop: 10,
-        }}
+        <section
+            className={styles.itemRow}
+        // style={{
+        //     display: "flex",
+        //     alignItems: "center",
+        //     gap: 10,
+        //     marginTop: 10,
+        // }}
         >
-            <span style={{
-                width: 150,
-            }}
+            <span className={styles['item-text']}
+                style={{
+                    color: quantity === 1 ? 'red' : 'black',
+                }}
             >{name}</span>
             <button onClick={handleAdd}>+1</button>
             <span>{count}</span>
