446810b1a4254e4c4efb089e276d78eba770a832
Martin Marmolejo <martinalejandro.marmolejo@gmail.com>
2026-08-28 16:04:28 -0700

    Se trabajo con Hook - useState

--- src/shopping-cart/ItemCounterList.tsx
+++ src/shopping-cart/ItemCounterList.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 
 interface Props {
     name: string;
     quantity?: number;
 }
 
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+
+    const [count, setCount] = useState(quantity)
+
+    const handleAdd = () => {
+        setCount(count + 1)
+    }
+    const handleSubtract = () => {
+        if (count === 1) return;
+        setCount(count - 1)
+    }
+
 
     const handleClick = () => {
         console.log(`click ${name}`)
@@ -23,9 +35,9 @@ export const ItemCounter = ({ name, quantity }: Props) => {
                 width: 150,
             }}
             >{name}</span>
-            <button onClick={handleClick}>+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
+            <button onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}>-1</button>
         </section>
     );
 };
